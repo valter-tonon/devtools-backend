0f8f0c500820fdbbd5a3dbcaa3dd423afc0ecad5
valter-tonon <tononvalter@gmail.com>
2022-04-10 17:34:44 -0300

    add sentry

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/debug-sentry', function () {
+    throw new Exception('My first Sentry error!');
+});
